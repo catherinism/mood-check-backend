ad874610fd49594a58cf7f2d898cd88f94da4222
catherinism <catcreates13@gmail.com>
2020-03-26 15:41:59 -0400

    add methods to moods and quotes controllers

--- app/controllers/api/v1/moods_controller.rb
+++ app/controllers/api/v1/moods_controller.rb
@@ -1,2 +1,23 @@
-class MoodsController < ApplicationController
+class Api::V1::MoodsController < ApplicationController
+
+    def index
+        @moods = Mood.all
+        render json: @moods
+    end
+
+    def show
+        @mood = Mood.find_by(id: params[:id])
+        render json: @mood
+    end
+
+    def create
+        mood = Mood.new(mood_params)
+        render json: mood
+    end
+
+    private
+    def mood_params
+        params.require(:mood).permit(:feeling, :url)
+    end
+    
 end
--- app/controllers/api/v1/quotes_controller.rb
+++ app/controllers/api/v1/quotes_controller.rb
@@ -1,2 +1,22 @@
-class QuotesController < ApplicationController
+class Api::V1::QuotesController < ApplicationController
+
+    def index
+        @quotes = Quote.all
+        render json: @quotes
+    end
+
+    def show
+        @quote = Quote.find_by(id: params[:id])
+        render json: @quote
+    end
+
+    def create
+        @quote = Quote.new(quote_params)
+    end
+
+    private
+    def quote_params
+        params.require(:quote).permit(:phrase, :mood_id)
+    end
+    
 end
